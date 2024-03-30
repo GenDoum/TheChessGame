6cee2631b36060abac4f450ecd0f2561c675a4a8
Mathéo Hersan <matheohersan@MacBook-Pro-de-Matheo.local>
2024-03-30 09:48:38 +0100

    Merging plateauPage

--- src/Chess/Chess/contentView/connexionModuleButtons.xaml.cs
+++ src/Chess/Chess/contentView/connexionModuleButtons.xaml.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Chess.contentView;
 
 public partial class connexionModuleButtons : ContentView

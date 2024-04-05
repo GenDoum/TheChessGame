35f7b64daa2910bc8b2223edaa7ab7281a3fc577
yannis.doumir_fernandes <yannis.doumir_fernandes@etu.uca.fr>
2024-04-05 17:26:29 +0200

    ADD classs player

---
+++ src/Chess/ChessLibrary/Player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLibrary
+{
+    /// <summary>
+    /// Classe Player
+    /// </summary>
+    public class Player
+    {
+        /// <summary>
+        /// Pseudo of the Player
+        /// </summary>
+        public string Pseudo { get; private set; }
+
+        /// <summary>
+        /// Mot de passe du Player
+        /// </summary>
+        private string Mdp;
+
+        /// <summary>
+        /// Contstructeur du Player
+        /// </summary>
+        /// <param name="pseudo"></param>
+        /// <param name="mdp"></param>
+        public Player(string pseudo, string mdp) 
+        {
+            Pseudo = pseudo;
+            Mdp = mdp;
+        }
+
+    }
+}

4193df66fd4759604ad5b7f7418d9212f559b2f7
matheo.balko <matheo.balko@etu.uca.fr>
2024-04-08 13:34:25 +0200

    a

--- src/Chess/ChessLibrary/Chessboard.cs
+++ src/Chess/ChessLibrary/Chessboard.cs
@@ -5,8 +5,55 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ChessLibrary
-{
-    internal class Chessboard
+{   public class Chessboard
     {
+        /// <summary>
+        /// Creation de la matrice pour stocké les cases 
+        /// </summary>
+        private Case[,] board = new Case[8,8];
+
+        /// <summary>
+        /// Implémente les cases dans le plateau.
+        /// </summary>
+        /// <param name="board"></param>
+        public Chessboard(Case[,] board)
+        {
+            string [] colonne = ["A", "B", "C", "D", "E", "F", "G", "H"];
+            int[] line = [1, 2, 3, 4, 5, 6, 7, 8];
+            for(int C=0; C < colonne.Length; C++) 
+            {
+                for (int l = 0; l < line.Length; l++)
+                {
+
+                    board[C, l] = new Case(colonne[C], line[l]);
+
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Creation d
+        /// </summary>
+        /// <param name="piece"></param>
+        /// <param name="cases"></param>
+
+        public void PlacerPiece(Piece piece, Case cases)
+        {
+            piece.case = cases;
+            for ()
+        }
+
+        /// <summary>
+        /// Creation d
+        /// </summary>
+        /// <param name="piece"></param>
+        /// <param name="cases"></param>
+
+        public void DeplacerPiece(Piece piece, Case cases)
+        {
+            piece.case = cases;
+
+        }
     }
 }

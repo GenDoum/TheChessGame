49ace89b4a029ce6f15e82ee989f69539362382c
yannis.doumir_fernandes <yannis.doumir_fernandes@etu.uca.fr>
2024-04-08 14:26:01 +0200

    ADD class User

--- src/Chess/ChessLibrary/User.cs
+++ src/Chess/ChessLibrary/User.cs
@@ -6,7 +6,115 @@ using System.Threading.Tasks;
 
 namespace ChessLibrary
 {
-    internal class User
+    /// <summary>
+    /// Classe Player
+    /// </summary>
+    public class User
     {
+
+
+        /// <summary>
+        /// Pseudo of the Player
+        /// </summary>
+        public string Pseudo
+        {
+            get => pseudo;
+            set
+            {
+                if (pseudo == value)
+                {
+                    return;
+                }
+               pseudo = value;
+            }
+        }
+        private string pseudo;
+
+        /// <summary>
+        /// Password of the Player
+        /// </summary>
+        public string Password
+        {
+            get => password;
+            set
+            {
+                if (password == value)
+                {
+                    return;
+                }
+                password = value;
+            }
+
+        }
+        private string password;
+
+        /// <summary>
+        /// Boolean for know if the player is in the white side or not
+        /// </summary>
+        public Color IsWhite;
+
+        /// <summary>
+        /// Constructor of Player with parameters
+        /// </summary>
+        /// <param name="pseudo"></param>
+        /// <param name="password"></param>
+        public User(string pseudo, string password, Color color)
+        {
+            Pseudo = pseudo;
+            Password = password;
+            Color IsWhite = color;
+        }
+
+        /// <summary>
+        /// Constructor of Player without paramaters.
+        /// This constructor will be used for the invited player.
+        /// </summary>
+        public User()
+        {
+            Pseudo = "Invité";
+            Password = null;
+        }
+
+
+        /// <summary>
+        /// Player's method to check the password
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public bool isPasswdConsole()
+        {
+
+            Console.WriteLine("Enter your password");
+            string password = Console.ReadLine();
+            if (Password == null)
+            {
+                Console.WriteLine("Invited player, no need to check password\n");
+                return true;
+            }
+
+            if (Password == password)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool isPasswd(string password)
+        {
+            if (Password == null)
+            {
+                Console.WriteLine("Invited player, no need to check password\n");
+                return true;
+            }
+
+            if (password.Equals(Password))
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
+
 }
--- src/Chess/ConsoleChess/Program.cs
+++ src/Chess/ConsoleChess/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Reflection.Metadata;
+using ChessLibrary;
+
+Console.WriteLine("Hello, World!");
+
+User a = new User();
+User b = new User("Jorge", "ouho", Color.Black);
+
+
+Console.WriteLine(a.Pseudo);
+Console.WriteLine(b.Pseudo);
 Console.WriteLine("Hello, World!");

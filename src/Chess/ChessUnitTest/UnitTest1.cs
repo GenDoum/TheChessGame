dfd3def1a35edc5af1e1ba7dc1ce5469268ebe9f
Matheo BALKO <matheo.balko@etu.uca.fr>
2024-04-16 13:05:35 +0200

    Mise à jour de 'src/Chess/ChessUnitTest/UnitTest1.cs'
    ajout de test unitaire pour le fou

--- src/Chess/ChessUnitTest/UnitTest1.cs
+++ src/Chess/ChessUnitTest/UnitTest1.cs
@@ -56,7 +56,89 @@ namespace ChessUnitTest
             var king = new King(Color.White, 1);
             Assert.Throws<InvalidOperationException>(() => king.canMove(1, 1, 3, 3));
         }
+        using Xunit;
+using ChessLibrary; // Assumez que c'est le namespace d'après le code fourni.
 
+public class BishopTests
+{
+    [Fact]
+    public void PossibleMoves_FromCenter_ReturnsAllDiagonalMoves()
+    {
+        // Initialisation
+        var board = new Case[8, 8];
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                board[i, j] = new Case(i, j); // Supposons que le constructeur initialise une case vide.
+            }
+        }
+        var chessboard = new Chessboard(board);
+        var bishop = new Bishop(Color.White, 1); // Supposons l'id '1' pour le fou.
+        var centerCase = board[4, 4];
+        board[4, 4].Piece = bishop; // Place le fou au centre.
+
+        // Action
+        var possibleMoves = bishop.PossibleMoves(centerCase, chessboard);
+
+        // Vérification
+        Assert.Contains(board[3, 3], possibleMoves);
+        Assert.Contains(board[5, 5], possibleMoves);
+        Assert.Contains(board[2, 6], possibleMoves);
+        Assert.Contains(board[6, 2], possibleMoves);
+        Assert.DoesNotContain(board[4, 4], possibleMoves); // La position actuelle du fou ne doit pas être incluse.
+    }
+
+    [Fact]
+    public void PossibleMoves_EdgeCase_DoesNotIncludeOutOfBoundsMoves()
+    {
+        // Initialisation
+        var board = new Case[8, 8];
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                board[i, j] = new Case(i, j);
+            }
+        }
+        var chessboard = new Chessboard(board);
+        var bishop = new Bishop(Color.White, 2);
+        var edgeCase = board[7, 7];
+        board[7, 7].Piece = bishop;
+
+        // Action
+        var possibleMoves = bishop.PossibleMoves(edgeCase, chessboard);
+
+        // Vérification
+        Assert.DoesNotContain(board[8, 8], possibleMoves); // Les mouvements hors limites ne doivent pas être inclus.
+    }
+
+    [Fact]
+    public void PossibleMoves_WithObstacles_StopsAtObstacles()
+    {
+        // Initialisation
+        var board = new Case[8, 8];
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                board[i, j] = new Case(i, j);
+            }
+        }
+        var chessboard = new Chessboard(board);
+        var bishop = new Bishop(Color.White, 3);
+        var startCase = board[4, 4];
+        board[4, 4].Piece = bishop;
+        board[6, 6].Piece = new Pawn(Color.Black, 4); // Obstacle au 6, 6.
+
+        // Action
+        var possibleMoves = bishop.PossibleMoves(startCase, chessboard);
+
+        // Vérification
+        Assert.Contains(board[6, 6], possibleMoves); // Le fou peut capturer au 6, 6.
+        Assert.DoesNotContain(board[7, 7], possibleMoves); // Les mouvements au-delà du pion ne sont pas inclus.
+    }
+}
 
     }
 }

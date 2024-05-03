1cfd196d8bc2d9d0305745d74ba410ca4d5a23dc
HMathéo <matheo.hersan@etu.uca.fr>
2024-05-03 18:04:16 +0200

    add doxyfile
    Signed-off-by: HMathéo <matheo.hersan@etu.uca.fr>

--- src/Chess/ConsoleChess/Program.cs
+++ src/Chess/ConsoleChess/Program.cs
@@ -18,65 +18,49 @@ namespace ConsoleChess
 
             Chessboard chessboard = new Chessboard(board);
 
-            while (true)
+        }
+
+        static void DisplayBoard(Chessboard chessboard)
+        {
+            Console.WriteLine("   a   b   c   d   e   f   g   h");
+            Console.WriteLine(" +---+---+---+---+---+---+---+---+");
+            for (int i = 0; i < 8; i++)
             {
-                DisplayBoard(chessboard);
-                Console.WriteLine("Enter the coordinates of the piece you want to move:");
-                Console.Write("X: ");
-                int x = int.Parse(Console.ReadLine());
-                Console.Write("Y: ");
-                int y = int.Parse(Console.ReadLine());
-
-                Case initialCase = chessboard.Board[x, y];
-                if (initialCase.Piece == null)
+                Console.Write((i + 1) + "|");
+                for (int j = 0; j < 8; j++)
                 {
-                    Console.WriteLine("There is no piece at this position.");
-                    continue;
-                }
-
-            }
-
-            static void DisplayBoard(Chessboard chessboard)
-            {
-                Console.WriteLine("   a   b   c   d   e   f   g   h");
-                Console.WriteLine(" +---+---+---+---+---+---+---+---+");
-                for (int i = 0; i < 8; i++)
-                {
-                    Console.Write((i + 1) + "|");
-                    for (int j = 0; j < 8; j++)
+                    Piece piece = chessboard.Board[j, i].Piece;
+                    if (piece != null)
                     {
-                        Piece piece = chessboard.Board[j, i].Piece;
-                        if (piece != null)
-                        {
-                            string pieceSymbol = GetPieceSymbol(piece);
-                            Console.Write(" " + pieceSymbol + " |");
-                        }
-                        else
-                        {
-                            Console.Write("   |");
-                        }
+                        string pieceSymbol = GetPieceSymbol(piece);
+                        Console.Write(" " + pieceSymbol + " |");
                     }
-
-                    Console.WriteLine((i + 1));
-                    Console.WriteLine(" +---+---+---+---+---+---+---+---+");
+                    else
+                    {
+                        Console.Write("   |");
+                    }
                 }
 
-                Console.WriteLine("   a   b   c   d   e   f   g   h");
+                Console.WriteLine((i + 1));
+                Console.WriteLine(" +---+---+---+---+---+---+---+---+");
             }
 
-            static string GetPieceSymbol(Piece piece)
+            Console.WriteLine("   a   b   c   d   e   f   g   h");
+        }
+
+        static string GetPieceSymbol(Piece piece)
+        {
+            switch (piece.GetType().Name)
             {
-                switch (piece.GetType().Name)
-                {
-                    case "Pawn": return "P";
-                    case "Rook": return "R";
-                    case "Knight": return "N";
-                    case "Bishop": return "B";
-                    case "Queen": return "Q";
-                    case "King": return "K";
-                    default: return "?";
-                }
+                case "Pawn": return "P";
+                case "Rook": return "R";
+                case "Knight": return "N";
+                case "Bishop": return "B";
+                case "Queen": return "Q";
+                case "King": return "K";
+                default: return "?";
             }
         }
+
     }
 }

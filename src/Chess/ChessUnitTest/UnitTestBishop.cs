c3cf6800d18aecdea378f9de6c8073b0f302b1ba
HMathéo <matheo.hersan@etu.uca.fr>
2024-05-04 08:35:19 +0200

    refractored InitialiseChessboard
    Signed-off-by: HMathéo <matheo.hersan@etu.uca.fr>

--- src/Chess/ChessUnitTest/UnitTestBishop.cs
+++ src/Chess/ChessUnitTest/UnitTestBishop.cs
@@ -42,4 +42,43 @@ public class UnitTestBishop
         // Assert
         Assert.Equal(13, result.Count);
     }
+    
+    [Fact]
+    public void PossibleMoves_NullChessboard_ThrowsException()
+    {
+        // Arrange
+        var bishop = new Bishop(Color.White, 1);
+        var caseInitial = new Case(4, 4, bishop);
+
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() => bishop.PossibleMoves(caseInitial, null));
+    }
+
+    [Fact]
+    public void PossibleMoves_NullCaseInitial_ThrowsException()
+    {
+        // Arrange
+        var bishop = new Bishop(Color.White, 1);
+        var chessboard = new Chessboard(new Case[8, 8],true);
+
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() => bishop.PossibleMoves(null, chessboard));
+    }
+    
+    [Fact]
+    public void PossibleMoves_NonEmptyCase_AddsToResult()
+    {
+        // Arrange
+        var bishop = new Bishop(Color.White, 1);
+        var chessboard = new Chessboard(new Case[8, 8],true);
+        var caseInitial = new Case(4, 4, bishop);
+        var otherPiece = new Bishop(Color.Black, 2);
+        chessboard.Board[5, 5] = new Case(5, 5, otherPiece);
+
+        // Act
+        var result = bishop.PossibleMoves(caseInitial, chessboard);
+
+        // Assert
+        Assert.Contains(chessboard.Board[5, 5], result);
+    }
 }

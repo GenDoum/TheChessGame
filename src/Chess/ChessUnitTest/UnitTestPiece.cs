8055fef4ca0cf5133a3215ce02666a6b3fd8ff7f
HMathéo <matheo.hersan@etu.uca.fr>
2024-05-06 15:07:23 +0200

    added a test

--- src/Chess/ChessUnitTest/UnitTestPiece.cs
+++ src/Chess/ChessUnitTest/UnitTestPiece.cs
@@ -3,4 +3,30 @@ using ChessLibrary;
 
 public class UnitTestPiece
 {
+    [Fact]
+    public void Constructor_ValidColor_SetsPropertiesCorrectly()
+    {
+        // Arrange
+        var color = Color.White;
+        var id = 1;
+
+        // Act
+        var piece = new Rook(color, id);
+
+        // Assert
+        Assert.Equal(color, piece.Color);
+        Assert.Equal(id, piece.id);
+    }
+
+    [Fact]
+    public void Constructor_InvalidColor_ThrowsArgumentException()
+    {
+        // Arrange
+        var color = (Color)999; // Invalid color
+        var id = 1;
+
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => new Rook(color, id));
+    }
 }
+

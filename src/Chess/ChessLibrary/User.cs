6914d35b6b8511deb3c942ab49a90206f4d72908
matheo.balko <matheo.balko@etu.uca.fr>
2024-05-10 21:46:56 +0200

    Correction des failure sur le sonnar

--- src/Chess/ChessLibrary/User.cs
+++ src/Chess/ChessLibrary/User.cs
@@ -36,150 +36,150 @@ namespace ChessLibrary
         }
         private string pseudo;
 
-        /// <summary>
-        /// Password of the Player
-        /// </summary>
-        //public string? Password
-        //{
-        //    get => password;
-        //    set
-        //    {
-        //        if (password == value)
-        //        {
-        //            return;
-        //        }
+//        /// <summary>
+//        /// Password of the Player
+//        /// </summary>
+//        //public string? Password
+//        //{
+//        //    get => password;
+//        //    set
+//        //    {
+//        //        if (password == value)
+//        //        {
+//        //            return;
+//        //        }
 
-        //        password = value;
+//        //        password = value;
 
-        //        if (string.IsNullOrWhiteSpace(Password))
-        //        {
-        //            throw new ArgumentException("Pseudo or password must be entered and must not be full of white space");
-        //        }
+//        //        if (string.IsNullOrWhiteSpace(Password))
+//        //        {
+//        //            throw new ArgumentException("Pseudo or password must be entered and must not be full of white space");
+//        //        }
 
-        //    }
+//        //    }
 
-        //}
-        //private string password;
+//        //}
+//        //private string password;
 
-        /// <summary>
-        /// Type for know the color of the player
-        /// </summary>
-        public Color color;
+//        /// <summary>
+//        /// Type for know the color of the player
+//        /// </summary>
+//        public Color color;
 
-        public int Score
-        {
-            get => score;
-            set
-            {
-                score = value;
-            }
-        }
+//        public int Score
+//        {
+//            get => score;
+//            set
+//            {
+//                score = value;
+//            }
+//        }
 
-        private int score;
+//        private int score;
 
-        /// <summary>
-        /// Constructor of Player with parameters
-        /// </summary>
-        /// <param name="pseudo"></param>
-        /// <param name="password"></param>
-        public User(string pseudo, Color color)
-        {
-            // En gros ici tu vérifiais si 'Pseudo' et 'Password' étaient null ou vide après leur affectation mais dcp les exceptions étaient pas levées avant que les valeurs soient déjà définies.
-            // J'ai aussi enlever IsWhite car il était jamais utilisé je crois. Remet si il servais vraiment.
-            // Et dcp la le constructeur vérifie d'abord si pseudo sont vide ou null avant de les affecter a la propriété Pseudo.
-            if (string.IsNullOrWhiteSpace(pseudo))
-            {
-                throw new ArgumentException("Pseudo or password must be entered and must not be full of white space");
-            }
+//        /// <summary>
+//        /// Constructor of Player with parameters
+//        /// </summary>
+//        /// <param name="pseudo"></param>
+//        /// <param name="password"></param>
+//        public User(string pseudo, Color color)
+//        {
+//            // En gros ici tu vérifiais si 'Pseudo' et 'Password' étaient null ou vide après leur affectation mais dcp les exceptions étaient pas levées avant que les valeurs soient déjà définies.
+//            // J'ai aussi enlever IsWhite car il était jamais utilisé je crois. Remet si il servais vraiment.
+//            // Et dcp la le constructeur vérifie d'abord si pseudo sont vide ou null avant de les affecter a la propriété Pseudo.
+//            if (string.IsNullOrWhiteSpace(pseudo))
+//            {
+//                throw new ArgumentException("Pseudo or password must be entered and must not be full of white space");
+//            }
 
-            Pseudo = pseudo;
-            //Password = password;
-            this.color = color;
-        }
+//            Pseudo = pseudo;
+//            //Password = password;
+//            this.color = color;
+//        }
 
-        /// <summary>
-        /// Constructor of Player without paramaters.
-        /// This constructor will be used for the invited player.
-        /// </summary>
-        public User()
-        {
-            Pseudo = "Invité";
-/*            Password = null;*/
-        }
+//        /// <summary>
+//        /// Constructor of Player without paramaters.
+//        /// This constructor will be used for the invited player.
+//        /// </summary>
+//        public User()
+//        {
+//            Pseudo = "Invité";
+///*            Password = null;*/
+//        }
 
-        /// <summary>
-        /// Player's method to check the password
-        /// </summary>
-        /// <returns>It return a boolean which say if the user has entering the good password of not</returns>
-        public bool isPasswdConsole()
-        {
+//        /// <summary>
+//        /// Player's method to check the password
+//        /// </summary>
+//        /// <returns>It return a boolean which say if the user has entering the good password of not</returns>
+//        public bool isPasswdConsole()
+//        {
 
-            Console.WriteLine($"Hello {Pseudo}, Enter your password please");
+//            Console.WriteLine($"Hello {Pseudo}, Enter your password please");
 
-            ConsoleKeyInfo key;
-            string pass = string.Empty;
-            key = System.Console.ReadKey(true);
+//            ConsoleKeyInfo key;
+//            string pass = string.Empty;
+//            key = System.Console.ReadKey(true);
 
-            while ( key.Key != ConsoleKey.Enter )
-            {
+//            while ( key.Key != ConsoleKey.Enter )
+//            {
 
-                if (key.Key != ConsoleKey.Backspace)
-                {
-                    pass += key.KeyChar;
-                    Console.Write("*");
-                }
-                else if (key.Key == ConsoleKey.Backspace && !string.IsNullOrEmpty(pass))
-                {
-                    // Supprime un élément de la liste de char de pass
-                    pass = pass.Substring(0, pass.Length - 1);
-                    // Récupère la position du curseur
-                    int pos = System.Console.CursorLeft;
-                    // Déplace le curseur d'un à gauche
-                    System.Console.SetCursorPosition(pos - 1, System.Console.CursorTop);
-                    // Remplace par un espace dans la console
-                    System.Console.Write(" ");
-                    // Déplace le curseur d'une position à gauche encore
-                    System.Console.SetCursorPosition(pos - 1, System.Console.CursorTop);
-                }
+//                if (key.Key != ConsoleKey.Backspace)
+//                {
+//                    pass += key.KeyChar;
+//                    Console.Write("*");
+//                }
+//                else if (key.Key == ConsoleKey.Backspace && !string.IsNullOrEmpty(pass))
+//                {
+//                    // Supprime un élément de la liste de char de pass
+//                    pass = pass.Substring(0, pass.Length - 1);
+//                    // Récupère la position du curseur
+//                    int pos = System.Console.CursorLeft;
+//                    // Déplace le curseur d'un à gauche
+//                    System.Console.SetCursorPosition(pos - 1, System.Console.CursorTop);
+//                    // Remplace par un espace dans la console
+//                    System.Console.Write(" ");
+//                    // Déplace le curseur d'une position à gauche encore
+//                    System.Console.SetCursorPosition(pos - 1, System.Console.CursorTop);
+//                }
 
-                key = System.Console.ReadKey(true);
+//                key = System.Console.ReadKey(true);
 
-            }
-/*            Console.WriteLine(pass);
-        if ( Equals( this.Password, null) )
-            {
-                Console.WriteLine("\nInvited player, no need to check password\n");
-                return true;
-            }
-            if ( Equals(this.Password, pass) )
-            {
-                Console.WriteLine($"\nGood password, have fun {Pseudo}");
-                return true;
-            }
-            else 
-            {
-                Console.WriteLine($"\nIt seems like you misswrite {Pseudo}, try again");
-                return false;
+//            }
+///*            Console.WriteLine(pass);
+//        if ( Equals( this.Password, null) )
+//            {
+//                Console.WriteLine("\nInvited player, no need to check password\n");
+//                return true;
+//            }
+//            if ( Equals(this.Password, pass) )
+//            {
+//                Console.WriteLine($"\nGood password, have fun {Pseudo}");
+//                return true;
+//            }
+//            else 
+//            {
+//                Console.WriteLine($"\nIt seems like you misswrite {Pseudo}, try again");
+//                return false;
 
-            }*/
-return true;
-        }
+//            }*/
+//return true;
+//        }
 
-/*        public bool isPasswd(string password)
-        {
-            if (Password == null)
-            {
-                Console.WriteLine("Invited player, no need to check password\n");
-                return true;
-            }
+///*        public bool isPasswd(string password)
+//        {
+//            if (Password == null)
+//            {
+//                Console.WriteLine("Invited player, no need to check password\n");
+//                return true;
+//            }
 
-            if (password.Equals(Password))
-            {
-                return true;
-            }
+//            if (password.Equals(Password))
+//            {
+//                return true;
+//            }
 
-            return false;
-        }*/
+//            return false;
+//        }*/
     }
 
 }

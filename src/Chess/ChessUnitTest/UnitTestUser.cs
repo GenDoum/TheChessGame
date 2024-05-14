5a0d6ded52e546f7db914a612e71550deac21232
yannis.doumir_fernandes <yannis.doumir_fernandes@etu.uca.fr>
2024-05-14 12:48:10 +0200

    MODIFY User and ADD test and TestData

--- src/Chess/ChessUnitTest/UnitTestUser.cs
+++ src/Chess/ChessUnitTest/UnitTestUser.cs
@@ -1,83 +1,116 @@
-﻿//namespace ChessUnitTest;
-//using ChessLibrary;
+﻿namespace ChessUnitTest;
+using ChessLibrary;
 
+public class UnitTestUser
+{
+    [Theory]
+    [MemberData(nameof(TestData.ValidUserPseudo), MemberType = typeof(TestData))]
+    public void ValidPseudo_ReturnTrue(string pseudo, string password, Color color, bool connected, List<Piece> pieces, int score)
+    {
+        if (string.IsNullOrWhiteSpace(pseudo))
+        {
+            Assert.Throws<ArgumentException>(() => new User(pseudo, password, color, connected, pieces, score));
+            return;
+        }
 
-//public class UnitTestUser
-//{
-//    [Theory]
-//    [MemberData(nameof(TestData.ValidUserPseudo), MemberType = typeof(TestData))]
-//    public void ValidPseudo_ReturnTrue(string pseudo, string password, Color color)
-//    {
-//        if (string.IsNullOrWhiteSpace(pseudo))
-//        {
-//            Assert.Throws<ArgumentException>(() => new User(pseudo, password, color));
-//            return;
-//        }
-    
-//        var user = new User(pseudo, password, color);
-    
-//    }
+        var user = new User(pseudo, password, color, connected, pieces, score);
 
-//    [Theory]
-//    [MemberData(nameof(TestData.InvalidUserPseudo), MemberType = typeof(TestData))]
-//    public void InvalidPseudo_ReturnFalse(string pseudo, string password, Color color)
-//    {
-//        if (string.IsNullOrWhiteSpace(pseudo))
-//        {
-//            Assert.Throws<ArgumentException>(() => new User(pseudo, password, color));
-//            return;
-//        }
+    }
 
-//        var user = new User(pseudo, password, color);
-//    }
+    [Theory]
+    [MemberData(nameof(TestData.InvalidUserPseudo), MemberType = typeof(TestData))]
+    public void InvalidPseudo_ReturnFalse(string pseudo, string password, Color color, bool connected, List<Piece> pieces, int score)
+    {
+        if (string.IsNullOrWhiteSpace(pseudo))
+        {
+            Assert.Throws<ArgumentException>(() => new User(pseudo, password, color, connected, pieces, score));
+            return;
+        }
 
+        var user = new User(pseudo, password, color, connected, pieces, score);
+    }
 
+    [Theory]
+    [MemberData(nameof(TestData.ValidUserPassword), MemberType = typeof(TestData))]
+    public void GoodPassword_ReturnTrue(string pseudo, string password, Color color, bool connected, List<Piece> pieces, int score)
+    {
+        var user = new User(pseudo, password, color, connected, pieces, score);
 
-//    [Theory] 
-//    [MemberData(nameof(TestData.ValidUserPassword), MemberType = typeof(TestData))]
-//    public void GoodPassword_ReturnTrue(string pseudo, string password, Color color)
-//    {
-//        var user = new User(pseudo, password, color);
+        if (password != null)
+        {
+            Assert.NotNull(password);
+        }
+        else
+        {
+            Assert.Null(password);
+        }
+    }
 
-//        if (password != null)
-//        {
-//            Assert.NotNull(password);
-//        }
-//        else
-//        {
-//            Assert.Null(password);
-//        }
-//    }
+    [Theory]
+    [MemberData(nameof(TestData.InvalidUserPassword), MemberType = typeof(TestData))]
+    public void InvalidPassword_ReturnFalse(string pseudo, string password, Color color, bool connected, List<Piece> pieces, int score)
+    {
+        Assert.Throws<ArgumentException>(() =>
+        {
+            var user = new User(pseudo, password, color, connected, pieces, score);
+        });
+    }
 
+    [Theory]
+    [MemberData(nameof(TestData.ValidUserColor), MemberType = typeof(TestData))]
+    public void GoodColor_ReturnTrue(string pseudo, string password, Color color, bool connected, List<Piece> pieces, int score)
+    {
+        var user = new User(pseudo, password, color, connected, pieces, score);
 
-//    [Theory]
-//    [MemberData(nameof(TestData.InvalidUserPassword), MemberType = typeof(TestData))]
-//    public void InvalidPassword_ReturnFalse(string pseudo, string password, Color color)
-//    {
-//        Assert.Throws<ArgumentException>(() =>
-//        {
-//            var user = new User(pseudo, password, color);
-//        });
-//    }
+        Assert.Equal(pseudo, user.Pseudo);
+        Assert.Equal(password, user.Password);
+        Assert.True(Equals(user.color, Color.White) || Equals(user.color, Color.Black));
+    }
 
-//    [Theory]
-//    [MemberData(nameof(TestData.ValidUserColor), MemberType = typeof(TestData))]
-//    public void GoodColor_ReturnTrue(string pseudo, string password, Color color)
-//    {
-//        var user = new User(pseudo, password, color);
+    [Theory]
+    [MemberData(nameof(TestData.InvalidUserColor), MemberType = typeof(TestData))]
+    public void WrongColor_ReturnTrue(string pseudo, string password, Color color, bool connected, List<Piece> pieces, int score)
+    {
+        var user = new User(pseudo, password, color, connected, pieces, score);
 
-//        Assert.Equal(pseudo, user.Pseudo);
-//        Assert.Equal(password, user.Password);
-//        Assert.True(Equals(user.color, Color.White) || Equals(user.color, Color.Black));
-//    }
+        Assert.NotNull(user.color);
+        Assert.True(Equals(user.color, Color.White) || Equals(user.color, Color.Black));
+    }
 
-//    [Theory]
-//    [MemberData(nameof(TestData.InvalidUserColor), MemberType = typeof(TestData))]
-//    public void WrongColor_ReturnTrue(string pseudo, string password, Color color)
-//    {
-//        var user = new User(pseudo, password, color);
+    [Theory]
+    [MemberData(nameof(TestData.ValidBoolConnected), MemberType = typeof(TestData))]
+    public void GoodBoolConnected_ReturnTrue(string pseudo, string password, Color color, bool connected, List<Piece> pieces, int score)
+    {
+        var user = new User(pseudo, password, color, connected, pieces, score);
 
-//        Assert.NotNull(user.color);
-//        Assert.True(Equals(user.color, Color.White) || Equals(user.color, Color.Black));
-//    }
-//}
+        Assert.True(Equals(user.IsConnected, true) || Equals(user.IsConnected, false));
+    }
+
+    [Theory]
+    [MemberData(nameof(TestData.InvalidBoolConnected), MemberType = typeof(TestData))]
+    public void WrongBoolConnected(string pseudo, string password, ChessLibrary.Color color, bool? connected, List<Piece> pieces, int score)
+    {
+        var user = new User(pseudo, password, color, connected ?? false, pieces, score);
+
+        Assert.Null(connected);
+    }
+
+    [Theory]
+    [MemberData(nameof(TestData.Test_UserMethodIsConnected), MemberType = typeof(TestData))]
+    public void TestGoodReturnMethodIsPassword(string password)
+    {
+        var user = new User("pseudo", password, Color.Black, false, new List<Piece>(), 0);
+
+        bool result = user.isPasswd(password);
+
+        if (password == user.Password)
+        {
+            Assert.True(result);
+        }
+        else
+        {
+            // If the password doesn't match, the result should be false
+            Assert.False(result);
+        }
+    }
+}

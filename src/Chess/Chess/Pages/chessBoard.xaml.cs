3f9b1e13103a1fef7d601af53b198d313568b840
HMathéo <matheo.hersan@etu.uca.fr>
2024-05-25 08:45:50 +0200

    started binding on board
    Signed-off-by: HMathéo <matheo.hersan@etu.uca.fr>

--- src/Chess/Chess/Pages/chessBoard.xaml.cs
+++ src/Chess/Chess/Pages/chessBoard.xaml.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ChessLibrary;
 
 namespace Chess.Pages;
 
 public partial class chessBoard : ContentPage
 {
+    public Chessboard Board { get; } = new Chessboard(new Case[8, 8], false);
     public chessBoard()
     {
         InitializeComponent();
+        BindingContext = this;
     }
 }

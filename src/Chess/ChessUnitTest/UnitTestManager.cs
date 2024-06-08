7dc491fb93a36f94ed6cf45fb64d71cb9bad11fc
unknown <matheo.balko@etu.uca.fr>
2024-06-08 23:43:11 +0200

    80%

---
+++ src/Chess/ChessUnitTest/UnitTestManager.cs
@@ -0,0 +1,47 @@
+﻿namespace ChessUnitTest;
+using ChessLibrary;
+using System.Collections.ObjectModel;
+
+public class UnitTestManager
+{
+    [Fact]
+    public void Games_GetterInitializesCollectionIfNull()
+    {
+        var Manager = new Manager();
+        // Arrange
+        var game = new Game();
+
+        // Act
+        var gamesCollection = Manager.Games;
+        var UserCollection = Manager.Users;
+        var ChessboardCollection = Manager.Chessboards;
+        // Assert
+        Assert.NotNull(gamesCollection);
+        Assert.IsType<ObservableCollection<Game>>(gamesCollection);
+        Assert.Empty(gamesCollection);
+        Assert.NotNull(UserCollection);
+        Assert.IsType<ObservableCollection<User>>(UserCollection);
+        Assert.Empty(UserCollection);
+        Assert.NotNull(ChessboardCollection);
+        Assert.IsType<ObservableCollection<Chessboard>>(ChessboardCollection);
+        Assert.Empty(ChessboardCollection);
+    }
+
+    [Fact]
+    public void Games_SetterAssignsNewCollectionAndRaisesPropertyChanged()
+    {
+        var Manager = new Manager();
+        // Arrange
+        var game = new Game();
+        var newGamesCollection = new ObservableCollection<Game>();
+
+
+        // Act
+        Manager.Games = newGamesCollection;
+
+        // Assert
+        Assert.Equal(newGamesCollection, Manager.Games);
+
+    }
+}
+

2a23ddd18d6000235df0c5292d444b6956b1a449
Matheo BALKO <matheo.balko@etu.uca.fr>
2024-06-12 19:36:00 +0200

    corection bug

--- src/Chess/Chess/contentView/viewLeaderBoard.xaml.cs
+++ src/Chess/Chess/contentView/viewLeaderBoard.xaml.cs
@@ -1,9 +1,29 @@
+using Chess.Pages;
+
 namespace Chess.contentView;
 
 public partial class viewLeaderBoard : ContentView
 {
-	public viewLeaderBoard()
-	{
-		InitializeComponent();
-	}
+
+    public static readonly BindableProperty PseudoProperty = BindableProperty.Create(nameof(Pseudo), typeof(string), typeof(viewLeaderBoard), default(string));
+    public static readonly BindableProperty ScoreWithSuffixProperty = BindableProperty.Create(nameof(ScoreWithSuffix), typeof(string), typeof(viewLeaderBoard), default(string));
+
+
+    public string Pseudo
+    {
+        get => (string)GetValue(PseudoProperty);
+        set => SetValue(PseudoProperty, value);
+    }
+    public string ScoreWithSuffix 
+    {
+        get => (string)GetValue(ScoreWithSuffixProperty);
+        set => SetValue(ScoreWithSuffixProperty, value);
+    }
+    public viewLeaderBoard()
+    {
+        InitializeComponent();
+    }
 }
+
+
+
